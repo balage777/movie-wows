475ce480458e7578d96821d9b15b2fb13bf439ea
balage777 <60267560+balage777@users.noreply.github.com>
2023-01-04 01:19:15 +0100

    Add files via upload

--- movie-wows-project/wow.js
+++ movie-wows-project/wow.js
@@ -3,19 +3,38 @@ var { createApp } = Vue;
 createApp({
   data() {
     return {
-      randomMovie: {'movie':'Random movie'},
+      randomMovie: null,
+      favourites: [],
       apiUrl : "https://owen-wilson-wow-api.onrender.com/wows/random",
     }
   },
   methods: {
-    async getRandomMovie() {
+    getRandomMovie: async function() {
         const result = await fetch(this.apiUrl);
         const data = await result.json();
         this.randomMovie = data[0];
-    }  
+    },
+    addFavourite: function() {
+      this.favourites.push(this.randomMovie);
+      this.saveToLocalStorage();
+    },
+    loadFavourite: function(movie) {
+      this.randomMovie = movie;
+    },
+    saveToLocalStorage() {
+      const parsed = JSON.stringify(this.favourites);
+      localStorage.setItem('favourites', parsed);
+    },
   },
   created() {
     this.getRandomMovie();
+    if (localStorage.getItem('favourites')) {
+      try {
+        this.favourites = JSON.parse(localStorage.getItem('favourites'));
+      } catch(e) {
+        localStorage.removeItem('favourites');
+      }
+    }
   }
 }).mount('#wow_app');
 
